4f36d1e457a1dad722c5654c95145fad72911a3f
Virjanand <virjanand@virjanand.com>
2020-06-17 06:18:26 +0200

    migrate junit4 tests to junit5

---
+++ src/test/java/guru/springframework/domain/NotesTest.java
@@ -0,0 +1,13 @@
+package guru.springframework.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NotesTest {
+
+    @Test
+    void getId() {
+        assertEquals(1, 1);
+    }
+}
